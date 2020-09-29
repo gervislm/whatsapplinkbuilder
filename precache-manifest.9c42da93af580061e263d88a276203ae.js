4b591470919156c8766db3dd03667bb766e7d225
Mackewinsson <mackewinsson@gmail.com>
2020-09-29 12:51:29 -0300

    Updates

---
+++ precache-manifest.9c42da93af580061e263d88a276203ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "533c908bbb1761bf58f11982d239788c",
+    "url": "/whatsapplinkbuilder/index.html"
+  },
+  {
+    "revision": "62b61cad1e5ece6f80fa",
+    "url": "/whatsapplinkbuilder/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "c3056f6e509e78e4d480",
+    "url": "/whatsapplinkbuilder/static/css/main.dda232dc.chunk.css"
+  },
+  {
+    "revision": "62b61cad1e5ece6f80fa",
+    "url": "/whatsapplinkbuilder/static/js/2.c2673c5c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/whatsapplinkbuilder/static/js/2.c2673c5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3056f6e509e78e4d480",
+    "url": "/whatsapplinkbuilder/static/js/main.27ce255b.chunk.js"
+  },
+  {
+    "revision": "fcb89992617c13577795",
+    "url": "/whatsapplinkbuilder/static/js/runtime-main.c3bf4646.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/whatsapplinkbuilder/precache-manifest.9c42da93af580061e263d88a276203ae.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/whatsapplinkbuilder/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
