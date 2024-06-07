33d797312afab1003867cf246ef27badf8bbec12
gervislm <gervislm@icloud.com>
2024-06-06 23:24:41 -0400

    FormComponent style improvements and logic changes

--- src/components/FormComponent/index.js
+++ src/components/FormComponent/index.js
@@ -6,76 +6,104 @@ import Image from "next/image";
 export const FormComponent = () => {
   const [text, setText] = useState("");
   const [number, setNumber] = useState("");
+  const [copyMessage, setCopyMessage] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(number);
   };
 
-  const copyLink = (e) => {
-    let link = document.querySelector(".link").innerText;
-    let linkNew = link.replace(/\++/g, "");
-    navigator.clipboard.writeText(linkNew);
-    let button = e.target;
-    button.style.color = "greenyellow";
-    button.innerText = "Copied";
-    const form = button.parentElement;
-    const tip = document.createElement("SPAN");
-    tip.innerText = "*** Whatsapp link is in your CLIPBOAD ***";
-    tip.style.color = "yellow";
-    form.insertBefore(tip, button);
-    setTimeout(() => {
-      button.style.color = "white";
-      button.innerText = "Copy";
-      form.removeChild(tip);
-    }, 1000);
+  const copyLink = () => {
+    const link = `api.whatsapp.com/send?phone=${number}&text=${encodeURIComponent(
+      text
+    )}`;
+    navigator.clipboard.writeText(link).then(() => {
+      setCopyMessage("Copied");
+      setTimeout(() => {
+        setCopyMessage("");
+      }, 2000);
+    });
+  };
+
+  const openLinkInNewTab = () => {
+    const link = `https://api.whatsapp.com/send?phone=${number}&text=${encodeURIComponent(
+      text
+    )}`;
+    window.open(link, "_blank");
   };
 
   return (
-    <div>
-      <div className="justify-content-center text-center">
-        <div xs={12} lg={6}>
-          <Image src={logo} alt="logo" />
-          <h1 className="title">Whatsapp link builder</h1>
-        </div>
+    <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <Image src={logo} alt="logo" className="mx-auto h-20 w-auto" />
+        <h1 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-white">
+          Whatsapp Link Builder
+        </h1>
       </div>
-      <div className="justify-content-center text-center">
-        <div xs={12} lg={6}>
-          <div className="link">{`api.whatsapp.com/send?phone=${number}&text=${text}`}</div>
-        </div>
+      <div className="chat chat-start mt-4">
+        <p className="chat-bubble text-wrap text-xl break-words ...">
+          {`api.whatsapp.com/send?phone=${number}&text=${encodeURIComponent(
+            text
+          )}`}
+        </p>
       </div>
-      <div className="justify-content-center">
-        <div xs={10}>
-          <form onSubmit={handleSubmit}>
-            <div controlId="formBasicNumber">
-              <label className="textlabel">Whatsapp Number</label>
+      <div className="w-full mx-auto max-w-7xl sm:px-6 lg:px-8">
+        <div className="col-10">
+          <form
+            onSubmit={handleSubmit}
+            className="flex flex-col justify-center items-center"
+          >
+            <div className="w-full mt-4 flex flex-col justify-center">
+              <label
+                className="text-xl font-bold leading-9 tracking-tight text-white"
+                htmlFor="whatsapp-number"
+              >
+                Whatsapp Number
+              </label>
               <input
+                id="whatsapp-number"
+                className="mt-1 text-black input input-bordered w-full"
                 type="text"
                 value={number}
                 placeholder="Enter whatsapp number"
                 onChange={(e) => setNumber(e.target.value)}
+                aria-label="Whatsapp Number"
               />
             </div>
 
-            <div controlId="formBasicText">
-              <label className="textlabel">Message</label>
-              <input
-                as="textarea"
+            <div className="w-full mt-4 flex flex-col justify-center">
+              <label
+                className="text-xl font-bold leading-9 tracking-tight text-white"
+                htmlFor="message-text"
+              >
+                Message
+              </label>
+              <textarea
+                id="message-text"
+                className="mt-1 text-black textarea textarea-bordered w-full"
                 placeholder="Enter text for your link"
                 rows={3}
                 value={text}
                 onChange={(e) => setText(e.target.value)}
+                aria-label="Message Text"
               />
             </div>
             <button
-              style={{ height: 100, marginTop: 50 }}
-              variant="primary"
-              size="lg"
-              block
+              type="button"
               onClick={copyLink}
-              className="animate__lightSpeedOutLeft"
+              className="btn btn-primary w-40 mt-10"
+              aria-live="polite"
             >
-              Copy
+              {copyMessage || "Copy"}
+            </button>
+            <button
+              type="button"
+              disabled={!text || !number}
+              onClick={openLinkInNewTab}
+              className="btn btn-secondary w-40 mt-10"
+              aria-live="polite"
+            >
+              Send
             </button>
           </form>
         </div>
